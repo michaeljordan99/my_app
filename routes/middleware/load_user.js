ca0d9ee3f45dfe10bfdeccb8e16daf1533bc2a74
Michael Jordan <michaeljordan99@hotmail.com>
2013-08-08 17:18:01 -0500

    added Mongoose

--- routes/middleware/load_user.js
+++ routes/middleware/load_user.js
@@ -1,11 +1,40 @@
 var users = require('../../data/users');
-var User = require('../../data/model/user')
+var User = require('../../data/model/user');
+var mongoose = require('mongoose');
 
 function loadUser(req, res, next) {
 	//var user = req.user = users[req.params.name];
 	debugger;
+
+	/* mongoose */
+	mongoose.connect('mongodb://localhost:27017');
+	
+	var db = mongoose.connection;
+	
+	db.once('open', function callback(){
+		console.log('mongo has opened!');
+	});
+
+	var userSchema = mongoose.Schema({
+	    name: String
+	});
+
+	var myUserModel = mongoose.model('MongoUser', userSchema);
+
+	var myUserInstance = new myUserModel({ name: 'I am an User Instance' });
+
+	console.log(myUserInstance.name);
+
+	myUserInstance.save(function (err, myUserInstance) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Check your database!')
+		}
+	});
+
 	var user = req.user = new User('Joey', 'Ramone');
-	console.log(user);
+
 	if(!user){
 		res.send('Not found', 404);
 	} else {
