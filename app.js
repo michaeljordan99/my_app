1afd40aa30945f73a267e721c407430c30caa9d0
Michael Jordan <michaeljordan99@hotmail.com>
2013-08-19 18:32:50 -0500

    updated per http://stackoverflow.com/questions/14322989/first-heroku-deploy-failed-error-code-h10

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ require('./routes/index')(app);
 require('./routes/users')(app);
 require('./routes/session')(app);
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
