0c362597dd7f7264ad55120777c43179caee25ee
Michael Jordan <michaeljordan99@hotmail.com>
2013-09-06 13:46:13 -0500

    added mocha tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+
+/* Array Test */
 var assert = require("assert")
 describe('Array', function(){
   describe('#indexOf()', function(){
@@ -6,4 +8,24 @@ describe('Array', function(){
       assert.equal(-1, [1,2,3].indexOf(0));
     })
   })
+})
+
+/* get_user tests */
+var get_user = require('../routes/middleware/get_user');
+describe('get_user', function(){
+	it('by id 1 should return Michael Jordan', function(){
+		var user = get_user.getById(1);
+		assert.equal(user.id, 1);
+		assert.equal(user.firstName, "Michael");
+		assert.equal(user.lastName, "Jordan");
+	})
+})
+
+describe('get_user', function(){
+	it('get_last should return Joe Smith', function(){
+		var user = get_user.getLast();
+		assert.equal(user.id, 2);
+		assert.equal(user.firstName, "Joe");
+		assert.equal(user.lastName, "Smith");
+	})
 })
